44c652de1853d994917cb20212ea4e7d3ccb1d15
Yassin <Yassin@DESKTOP-QC5MCK8.lan>
2020-04-29 18:46:28 +0200

    Final POS application

--- Seminar3.pointOfSale/src/se/kth/iv1350/pointOfSale/startup/Main.java
+++ Seminar3.pointOfSale/src/se/kth/iv1350/pointOfSale/startup/Main.java
@@ -6,8 +6,18 @@ import se.kth.iv1350.pointOfSale.integration.Printer;
 import se.kth.iv1350.pointOfSale.integration.RegistryHandler;
 import se.kth.iv1350.pointOfSale.view.View;
 
+/**
+ * Contains the <code> main <code> method. This method starts up the application.
+ * @author Yassin
+ *
+ */
 public class Main {
-
+	
+	/**
+	 * Starts the application.
+	 * 
+	 * @param args The application does not take any command line parameters.
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Printer printer = new Printer();
--- Seminar3.pointOfSale/test/se/kth/iv1350/pointOfSale/view/ViewTest.java
+++ Seminar3.pointOfSale/test/se/kth/iv1350/pointOfSale/view/ViewTest.java
@@ -2,7 +2,6 @@ package se.kth.iv1350.pointOfSale.view;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
